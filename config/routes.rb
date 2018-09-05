aa8c23d7b05b1d7ff1f7374bc23d98bda8fc0f68
kasiditbualoeng <subdit@gmail.com>
2018-09-05 14:14:46 -0400

    check resources

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,7 @@ Rails.application.routes.draw do
   post '/sign-in' => 'users#signin'
   delete '/sign-out' => 'users#signout'
   patch '/change-password' => 'users#changepw'
+#  post '/createFood' => 'users#createFood'
+#  delete '/deleteFood' => 'users#destroy'
+#  patch '/updateFood' => 'users#update'
 end
