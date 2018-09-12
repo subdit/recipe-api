da185d6e942ea9b88fc2c213051a09d48c5a1436
kasiditbualoeng <subdit@gmail.com>
2018-09-12 12:55:10 -0400

    Change foods_controller and  food_serializer

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class FoodsController < OpenReadController
+class FoodsController < ProtectedController
   before_action :set_food, only: %i[show update destroy]
 
   # GET /examples
